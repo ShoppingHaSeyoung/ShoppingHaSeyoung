21b9c80b82e3e9c8fb8cd9a604c9a1d329028667
selffy <105491566+selffy@users.noreply.github.com>
2023-01-02 19:41:03 +0900

    create project

---
+++ src/main/java/exit/yungyong/shopping/ShoppingHaseApplication.java
@@ -0,0 +1,13 @@
+package exit.yungyong.shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingHaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingHaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/exit/yungyong/shopping/ShoppingHaseApplicationTests.java
@@ -0,0 +1,13 @@
+package exit.yungyong.shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingHaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
